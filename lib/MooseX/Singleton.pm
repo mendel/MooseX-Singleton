39bb71dd248b61723d659ad212c0ea14b459464f
Dave Rolsky <autarch@urth.org>
2008-09-01 16:43:48 +0000

    bumper version to 0.10 and depend on Moose 0.56

--- lib/MooseX/Singleton.pm
+++ lib/MooseX/Singleton.pm
@@ -5,7 +5,7 @@ use Moose::Exporter;
 use MooseX::Singleton::Object;
 use MooseX::Singleton::Meta::Class;
 
-our $VERSION = '0.09_02';
+our $VERSION = '0.10';
 $VERSION = eval $VERSION;
 
 Moose::Exporter->setup_import_methods( also => 'Moose' );
