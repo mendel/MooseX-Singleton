e092ebe0b342e716490166f78037d40916edeaba
Dave Rolsky <autarch@urth.org>
2008-12-08 17:10:45 +0000

    bump version to 0.13

--- lib/MooseX/Singleton.pm
+++ lib/MooseX/Singleton.pm
@@ -5,7 +5,7 @@ use Moose::Exporter;
 use MooseX::Singleton::Object;
 use MooseX::Singleton::Meta::Class;
 
-our $VERSION = '0.12';
+our $VERSION = '0.13';
 $VERSION = eval $VERSION;
 
 Moose::Exporter->setup_import_methods( also => 'Moose' );
