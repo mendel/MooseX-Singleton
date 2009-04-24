7ddec692a68ee486c38ad7a6d1629059ff807746
Shawn M Moore <sartak@gmail.com>
2009-04-24 13:57:31 -0400

    Begin 0.17 releng

--- lib/MooseX/Singleton.pm
+++ lib/MooseX/Singleton.pm
@@ -5,7 +5,7 @@ use Moose::Exporter;
 use MooseX::Singleton::Object;
 use MooseX::Singleton::Meta::Class;
 
-our $VERSION = '0.16';
+our $VERSION = '0.17';
 $VERSION = eval $VERSION;
 
 Moose::Exporter->setup_import_methods( also => 'Moose' );
