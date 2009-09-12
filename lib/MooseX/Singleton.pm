c77ec30d3f650b696438bc3dff1544f193efb54c
Dave Rolsky <autarch@urth.org>
2009-09-12 09:50:23 -0500

    update changes and bump version to 0.21

--- lib/MooseX/Singleton.pm
+++ lib/MooseX/Singleton.pm
@@ -6,7 +6,7 @@ use MooseX::Singleton::Role::Object;
 use MooseX::Singleton::Role::Meta::Class;
 use MooseX::Singleton::Role::Meta::Instance;
 
-our $VERSION = '0.20';
+our $VERSION = '0.21';
 $VERSION = eval $VERSION;
 
 Moose::Exporter->setup_import_methods( also => 'Moose' );
